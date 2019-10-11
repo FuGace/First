82d68b695a86dc5e54f2f780d0bfac25932f2bec
FuGace <yuevs2@mail.ru>
2019-10-11 18:47:48 +0300

    Немного порадобал со стрелочками товаров, пробовал сделать еще один слайдер

--- JS/script.js
+++ JS/script.js
@@ -29,3 +29,24 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className+= " active"
 }
+
+
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("goods_container_inside");
+//     var dots = document.getElementsByClassName("designers-featured-latest");
+//     if (n > slides.length) {
+//       slideIndex = 1
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length
+//     }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "flex";
+//     dots[slideIndex - 1].className += " active";
+// }
