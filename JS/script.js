4a36cf5bf83943ccb0c0a7d545765349a1c33f7c
FuGace <yuevs2@mail.ru>
2019-10-12 20:03:25 +0300

    Мелочи

--- JS/script.js
+++ JS/script.js
@@ -50,3 +50,19 @@ function showSlides(n) {
 //     slides[slideIndex - 1].style.display = "flex";
 //     dots[slideIndex - 1].className += " active";
 // }
+
+$(function(){
+	
+	$('.hamburger_icon').on('click', function(){
+		
+		$('.menu_clearfix').slideToggle(300, function(){
+			
+			if($(this).css('display') === 'none'){
+				$(this).removeAttr('style');
+		}
+		
+	});
+	
+ });
+	
+});
