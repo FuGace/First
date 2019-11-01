d29f44f8bcdf16606ff1f13566c3ea1a6b651d8e
FuGace <yuevs2@mail.ru>
2019-11-01 14:34:06 +0300

    Небольшие правки в структуре и названиях классов

--- JS/script.js
+++ JS/script.js
@@ -13,7 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("container__inner__slider_first");
+    let slides = document.getElementsByClassName("container__inner__slider_crazy");
     let dots = document.getElementsByClassName("container__dot");
 
     if (n >slides.length) {
