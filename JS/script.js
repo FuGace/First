88758089269a5ad15825aa80a6e24a9c69741702
FuGace <yuevs2@mail.ru>
2019-11-10 14:09:18 +0300

    Добавил слайдер

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,66 @@
 'use strikt'
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndexCrazy = 1;
+showSlidesCrazy(slideIndexCrazy);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusSlidesCrazy(n) {
+    showSlidesCrazy(slideIndexCrazy += n);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function currentSlideCrazy(n) {
+    showSlidesCrazy(slideIndexCrazy = n);
 }
 
-function showSlides(n) {
+function showSlidesCrazy(n) {
     let i;
-    let slides = document.getElementsByClassName("container__inner__slider_crazy");
-    let dots = document.getElementsByClassName("container__dot");
+    let slidesCrazy = document.getElementsByClassName("container__inner__slider_crazy");
+    let dotsCrazy = document.getElementsByClassName("container__dot");
 
-    if (n >slides.length) {
-        slideIndex = 1
+    if (n >slidesCrazy.length) {
+        slideIndexCrazy = 1
     }
     if (n < 1){
-        slideIndex=slides.length
+        slideIndexCrazy = slidesCrazy.length
     }
-    for (i = 0; i <slides.length ; i++){
-        slides[i].style.display= "none"
+    for (i = 0; i <slidesCrazy.length ; i++){
+        slidesCrazy[i].style.display= "none"
     }
-    for (i = 0; i < dots.length; i++){
-        dots[i].className= dots[i].className.replace("active","")
+    for (i = 0; i < dotsCrazy.length; i++){
+        dotsCrazy[i].className= dotsCrazy[i].className.replace("active","")
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className+= " active"
+    slidesCrazy[slideIndexCrazy-1].style.display = "block";
+    dotsCrazy[slideIndexCrazy-1].className+= " active"
+}
+
+
+let slideIndexGoods = 1;
+showSlidesGoods(slideIndexGoods);
+
+function plusSlidesGoods(n) {
+    showSlidesGoods(slideIndexGoods += n);
+}
+
+function currentSlideGoods(n) {
+    showSlidesGoods(slideIndexGoods = n);
+}
+
+function showSlidesGoods(n) {
+    let i;
+    let slidesGoods = document.getElementsByClassName("goods__container__inside_slider");
+    let tabGoods = document.getElementsByClassName("container__inner__menu-designers__links-text");
+
+    if (n > slidesGoods.length) {
+        slideIndexGoods = 1
+    }
+    if (n < 1){
+        slideIndexGoods = slidesGoods.length
+    }
+    for (i = 0; i < slidesGoods.length ; i++){
+        slidesGoods[i].style.display = "none"
+    }
+    for (i = 0; i < tabGoods.length; i++){
+        tabGoods[i].className = tabGoods[i].className.replace("active_goods","")
+    }
+    slidesGoods[slideIndexGoods-1].style.display = "flex";
+    tabGoods[slideIndexGoods-1].className+= " active_goods"
 }
